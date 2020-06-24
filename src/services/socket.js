f8720e80cc37d0ce32139f8e0f7bad704e6b329f
Charles Sal <charles.asal@outlook.com>
2020-06-24 17:48:15 -0300

    initial commit

---
+++ src/services/socket.js
@@ -0,0 +1,35 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.1.110:3333', {
+  autoConnect: false,
+});
+
+function subscribeToNewDevs(subscribeFunction) {
+  socket.on('new-dev', subscribeFunction);
+}
+
+function connect(latitude, longitude, techs) {
+  socket.io.opts.query = {
+    latitude,
+    longitude,
+    techs,
+  };
+
+  socket.connect();
+
+  socket.on('message', text => {
+
+  })
+}
+
+function disconnect() {
+  if (socket.connect) {
+    socket.disconnect();
+  }
+}
+
+export {
+  connect,
+  disconnect,
+  subscribeToNewDevs,
+};
